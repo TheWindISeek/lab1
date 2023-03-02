2f6d670bf68e3b9fa4cabae1b7ac7b01aac3ca40
JeffreySharp <FAIZ030612@163.com>
2023-03-02 18:44:52 +0800

    完成了基础的功能 做了一个异常处理

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,34 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trans = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Location = new System.Drawing.Point(147, 105);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.Location = new System.Drawing.Point(3, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "摄氏度";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(64, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "华氏";
+            // 
+            // trans
+            // 
+            this.trans.BackColor = System.Drawing.SystemColors.GrayText;
+            this.trans.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.trans.Location = new System.Drawing.Point(263, 48);
+            this.trans.Name = "trans";
+            this.trans.Size = new System.Drawing.Size(66, 30);
+            this.trans.TabIndex = 5;
+            this.trans.Text = "转化";
+            this.trans.UseVisualStyleBackColor = false;
+            this.trans.Click += new System.EventHandler(this.trans_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(350, 135);
+            this.Controls.Add(this.trans);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "摄氏温度转华氏温度";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button trans;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,9 +18,28 @@ namespace lab1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            Debug.WriteLine("form1");
+        }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("label1");
+        }
+
+        private void trans_Click(object sender, EventArgs e)
+        {
+            String s = textBox1.Text;
+            double i = 0;
+            try
+            {
+                i = Convert.ToDouble(s);
+            }catch (Exception ex)
+            {
+                ex.Source = "Error";
+            }
+            textBox2.Text = (32 + 9 / 5 * i).ToString();
         }
     }
 }
